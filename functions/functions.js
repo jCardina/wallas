40ae05102fe24e26f0bb7220460b855945046c2f
jCardina <60352221+jCardina@users.noreply.github.com>
2020-07-17 21:01:57 -0300

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -3,3 +3,10 @@
 let goToContactDetails = document.getElementById("contactBtn").addEventListener("click", function(event) {
 	window.location.href = "#contacto";
 });
+
+
+let toggleHamMenu = document.getElementById("menuBtn").addEventListener("click", function(event) {
+
+	this.classList.toggle("change");
+
+});
