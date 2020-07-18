38ab55e6cb8c5616b09eb276cfe181ba0d5da0f2
jCardina <60352221+jCardina@users.noreply.github.com>
2020-07-17 21:23:20 -0300

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -8,5 +8,8 @@ let goToContactDetails = document.getElementById("contactBtn").addEventListener(
 let toggleHamMenu = document.getElementById("menuBtn").addEventListener("click", function(event) {
 
 	this.classList.toggle("change");
+	document.getElementById("menu").classList.toggle("open");
+
+	// cerrar menu clickeando en links
 
 });
