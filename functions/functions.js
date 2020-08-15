d5c627f4d715ac8580699b91c6d6f550c0631442
jCardina <60352221+jCardina@users.noreply.github.com>
2020-08-15 17:41:56 -0300

    animations

--- functions/functions.js
+++ functions/functions.js
@@ -12,7 +12,7 @@ let toggleHamMenu = document.getElementById("menuBtn").addEventListener("click",
 
 });
 
-let services = Array.from(document.querySelectorAll(".services div"));
+let services = document.querySelectorAll(".services div");
 
 let openServiceDetails = function() {
 
@@ -51,7 +51,85 @@ let closeMenuOnSelection = function() {
 	}
 }
 
+
+
+
+// let scroll = window.requestAnimationFrame ||
+// function(callback){ window.setTimeout(callback, 1000/60)};
+
+// let elementsToShow = document.querySelectorAll('.services div');
+
+// let loop = function() {
+
+// 	elementsToShow.forEach(element => {
+
+// 		if (isElementInViewport(element)) {
+// 			element.classList.add('is-visible');
+// 		} else {
+// 			element.classList.remove('is-visible');
+// 		}
+// 	});
+
+// 	scroll(loop);
+// }
+
+// const callback = function(entries) {
+//   entries.forEach(entry => {
+//     entry.target.classList.toggle("is-visible");
+//   });
+// };
+
+// const observer = new IntersectionObserver(callback);
+
+// const targets = document.querySelectorAll(".services div");
+// targets.forEach(function(target) {
+//   observer.observe(target);
+// });
+
+
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.services div'); 
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+
+function isElementInViewport(element) {
+  
+  let rect = element.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+
+
+
+
 closeMenuOnSelection();
 openServiceDetails();
+// loop();
 
 // add auto-close menu
